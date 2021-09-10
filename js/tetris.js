18ab113718e78184ecb273e018380e5b79f6d23b
nan-noo <54002105+nan-noo@users.noreply.github.com>
2021-09-10 22:23:03 +0900

    complete basic game function(move block, pause, restart, score, ...))
    move blocks with arrow key
    rotate blocks with up arrow key
    pause game with p
    restart game with r

--- js/tetris.js
+++ js/tetris.js
@@ -1,5 +1,11 @@
+import BLOCKS from './blocks.js';
+
 // DOM
 const container = document.querySelector(".container > ul");
+const gameScore = document.querySelector(".score > h3");
+const modal = document.querySelector(".modal");
+const modalText = document.querySelector(".modal-text");
+const modalBtn = document.querySelector(".modal-button");
 
 // settings
 const ROWS = 20;
@@ -9,19 +15,10 @@ let score = 0;
 let duration = 500;
 let downInterval;
 let movingItemNext;
-
-// type -> direction -> position
-const BLOCKS = {
-    tree: [
-        [[1,0], [0,1], [1,1], [1,2]],
-        [],
-        [],
-        []
-    ],
-}
+let isPause;
 
 const movingItem = {
-    type: "tree",
+    type: "",
     direction: 0,
     top: 0,
     left: 3,
@@ -29,15 +26,15 @@ const movingItem = {
 
 // functions
 const init = () => {
+    container.innerHTML = '';
     for(let i = 0; i < ROWS; i++){
-        appendRows();
+        prependRows();
     }
+    isPause = false;
+    generateNewBlocks();
+};
 
-    movingItemNext = { ...movingItem };
-    renderBlocks();
-}
-
-const appendRows = () => {
+const prependRows = () => {
     const row = document.createElement("li");
     const rowContent = document.createElement("ul");
     for(let j = 0; j < COLS; j++){
@@ -45,11 +42,71 @@ const appendRows = () => {
         rowContent.append(cell);
     }
     row.append(rowContent);
-    container.appendChild(row);
-}
+    container.prepend(row);
+};
 
-const renderBlocks = () => {
-    let {type, direction, top, left} = movingItemNext;
+const generateNewBlocks = () => {
+    // basic block down
+    clearInterval(downInterval);
+    downInterval = setInterval(() => {
+        moveBlocks('top', 1);
+    }, duration);
+
+    // new block
+    const boxArray = Object.keys(BLOCKS);
+    const type = boxArray[Math.floor(Math.random() * boxArray.length)];
+    movingItem.type = type;
+    movingItem.direction = 0;
+    movingItem.top = 0;
+    movingItem.left = 3;
+    movingItemNext = { ...movingItem };
+    renderBlocks();
+};
+
+const stopBlocks = () => {
+    // make moving blocks to stop
+    const movingBlocks = document.querySelectorAll(".moving");
+    movingBlocks.forEach(block => {
+        block.classList.remove("moving");
+        block.classList.add("stop");
+    })
+    checkLine();
+};
+
+const checkLine = () => {
+    // check if some lines are completed
+    const childNodes = container.childNodes; // rows(li)
+    childNodes.forEach(child => {
+        let lineCompleted = true;
+        for(let li of child.childNodes[0].childNodes){
+            if(!li.classList.contains("stop")){
+                lineCompleted = false;
+                break;
+            }
+        }
+        if(lineCompleted){
+            child.remove();
+            prependRows();
+            score += 10;
+        }
+    });
+    gameScore.innerHTML = score;
+    if(score > 0){
+        clearInterval(downInterval);
+        modalText.innerHTML = "Congratulations!!"
+        modalBtn.innerHTML = "RESTART"
+        modal.style.display = "flex";
+        modalBtn.onclick = () => {
+            modal.style.display = "none";
+            init();
+        };
+        return;
+    }
+    generateNewBlocks();
+};
+
+const renderBlocks = (moveType = '') => {
+    const {type, direction, top, left} = movingItemNext;
 
     // remove current blocks before update
     const movingBlocks = document.querySelectorAll(".moving");
@@ -57,46 +114,105 @@ const renderBlocks = () => {
         block.classList.remove(type, "moving");
     })
 
-    // update next position
-    for(let pos of BLOCKS[type][direction]){
+    // update block position
+    BLOCKS[type][direction].some(pos => {
         const x = pos[0] + top; // row
         const y = pos[1] + left; // col
 
         const target = container.childNodes[x] 
             && container.childNodes[x].childNodes[0].childNodes[y];
             
-        if(!!target) target.classList.add(type, "moving");
-        else{
-            // back to last state
-            movingItemNext = { ...movingItem };
-            renderBlocks();
-            break;
+        if(!!target && !target.classList.contains("stop")) {
+            target.classList.add(type, "moving");
         }
-    }
-    movingItem.direction = movingItemNext.direction;
-    movingItem.left = movingItemNext.left;
-    movingItem.top = movingItemNext.top;
-}
+        else{ // no target or stop block
+            if(moveType === 're-rendering'){
+                gameOver();
+                return true;
+            }
+
+            // back to previous state
+            movingItemNext = { ...movingItem };
+            setTimeout(() => {
+                renderBlocks('re-rendering');
+                if(moveType === 'top') stopBlocks();
+            }, 0);
+            return true;
+        }
+    });
+    movingItem.direction = direction;
+    movingItem.left = left;
+    movingItem.top = top;
+};
 
 const moveBlocks = (moveType, amount) => {
     movingItemNext[moveType] += amount;
+    renderBlocks(moveType);
+};
+
+const dropBlocks = () => {
+    if(isPause) return;
+    clearInterval(downInterval);
+    downInterval = setInterval(() => {
+        moveBlocks('top', 1); 
+    }, 10);
+};
+
+const pauseBlocks = () => {
+    isPause = !isPause;
+    if(isPause){
+        clearInterval(downInterval);
+        modalText.innerHTML = "Pause"
+        modalBtn.innerHTML = "CONTINUE"
+        modal.style.display = "flex";
+        modalBtn.onclick = () => {
+            isPause = !isPause;
+            modal.style.display = "none";
+
+            clearInterval(downInterval);
+            downInterval = setInterval(() => {
+                moveBlocks('top', 1);
+            }, duration);
+        };
+    } 
+};
+
+const changeDirection = () => {
+    if(isPause) return;
+    movingItemNext.direction = (movingItemNext.direction + 1) % 4;
     renderBlocks();
-}
+};
 
+const gameOver = () => {
+    clearInterval(downInterval);
+    modalText.innerHTML = "Game Over"
+    modalBtn.innerHTML = "RESTART"
+    modal.style.display = "flex";
+    modalBtn.onclick = () => {
+        modal.style.display = "none";
+        init();
+    }
+};
 
-//
+// start
 init();
+modalBtn.onclick = () => {
+    modal.style.display = "none";
+    init();
+};
 
-// event
+// event handling
 document.addEventListener("keydown", e => {
-    const keys = {
-        "ArrowUp"(){ moveBlocks('top', -1); },
+    const codes = {
+        "ArrowUp"(){ changeDirection(); },
         "ArrowDown"(){ moveBlocks('top', 1); },
         "ArrowRight"(){ moveBlocks('left', 1); },
         "ArrowLeft"(){ moveBlocks('left', -1); },
+        "Space"(){ dropBlocks(); },
+        "KeyP"(){ pauseBlocks(); },
+        "KeyR"(){ init(); },
     }
-
-    if(Object.keys(keys).includes(e.key)) keys[e.key]();
-})
+    if(Object.keys(codes).includes(e.code)) codes[e.code]();
+});
     
 
